a512dcb82ca23fc10a104d0fe6e718a7b1cc0339
Amos Yeo <amosyeo.sh@gmail.com>
2025-02-09 19:04:08 +0800

    Working now. Re-organise later

--- app.js
+++ app.js
@@ -5,34 +5,27 @@ const mongoose = require('mongoose');
 mongoose.set("debug", true)
 
 
-const Customer = require("./mondels/Customer")
+const Customer = require("./models/Customer")
 const prompt = require('prompt-sync')();
-
+const log = require("debug")("intro-to-mongoose-lab:app.js")
 
 const connect = async () => {
   
 
   await mongoose.connect(process.env.MONGODB_URI);
   console.log("Connected to MongoDB");
-  // await runQueries();
+  await runQueries();
 
   await mongoose.disconnect();
   console.log("Disconnected from MongoDB");
   process.exit();
 };
-
-connect().catch(err => {
-  console.error('Error:', err);
-  process.exit(1);
-});
-
-
-
+connect();
 
 const createCustomer = async () =>{
   const custName = prompt("What is the customers new name?")
-  console.log(`Customers name is: ${custName}`)
   const custAge = prompt("What is the customers new age?")
+  console.log(`Customers name is: ${custName}`)
   console.log(`Customers age is: ${custAge}`)
 
     const custData = {
@@ -42,21 +35,65 @@ const createCustomer = async () =>{
 
     const customer = await Customer.create(custData);
 
-    console.log("New Customer: ", customer)
+    console.log("New Customer: ", customer.name)
+    const next = prompt("Press any enter to continue.")
+    
+}
 
+
+const viewAllCustomer = async () =>{
+  // console.log(`View All Customers HERE!`)
+  const customers = await Customer.find({})
+
+  if(customers.length >= 1){
+    console.log(`
+      Below is a list of customers:`)
+      customers.forEach((customer)=>{
+        console.log(`
+          Name: ${customer.name}
+          Age: ${customer.age}
+          ID: ${customer._id}
+          `)
+      })
+  } else {
+    return console.log(`No customers. `)
+  }
+  const next = prompt("Press any enter to continue.")
+}
+
+
+const updateCustomer = async () => {
+  await viewAllCustomer();
+  const custId = prompt(`Copy and paste the id of the customer you would like to update here:`);
+  const newCustName = prompt(`What is the customers new name?`);
+  const newCustAge = prompt(`What is the customers new age?`);
+  const updateCust = await Customer.findById(custId);
+
+  updateCust.name = newCustName;
+  updateCust.age = newCustAge;
+
+  await updateCust.save()
+  const next = prompt("Press any enter to continue.")
+}
+
+const deleteCustomer = async () => {
+  await viewAllCustomer();
+  const custId = prompt(`Copy and paste the id of the customer you would like to delete here:`);
+  const delCust = await Customer.findById(custId);
+  const displayDelName = delCust.name
+  await delCust.deleteOne();                    // auto saves after delete. No save required. 
+  // await delCust.save();                      // Cant save after its been deleted. 
+  console.log(`Customer "${displayDelName}" has been deleted.`)
+  const next = prompt("Press any enter to continue.")
 }
 
 
 
 
-// configs
-const log = require("debug")("intro-to-mongoose-lab:app.js")
 
-mongoose.connect(process.env.MONGODB_URI)
-mongoose.set("debug", true)
-mongoose.connection.on("connected", () => {
-  log(`Connected to MongoDB ${mongoose.connection.name}.`);
-});
+
+// configs
+
 
 // middleware
 // app.use(morgan("dev"))
@@ -80,7 +117,7 @@ const runQueries = async () =>{
 
       Number of action to run:
       `)
-
+      //https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Statements/switch
       switch(start){
         case '5':
           appStatus = false;
@@ -89,26 +126,26 @@ const runQueries = async () =>{
           break;
         case '1':
           console.log(`Selected: Creating customer`)
-          createCustomer();
+          await createCustomer();
           break;
         case '2':
           console.log(`Selected: View all customers`)
+          await viewAllCustomer();
           break;
         case '3':
           console.log(`Selected:  Update a customer`)
+          await updateCustomer();
           break;
         case '4':
           console.log(`Selected: Delete a customer`)
+          await deleteCustomer();
           break;
+        default:
+          console.log(`Please choose between 1-5.`)
+          const next = prompt("Press any keys to continue.")
+          break;      
       }
-
-
-
-
-
-  
   }
-
 }
 
 
